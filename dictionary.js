dc79c0a99e1456168847ab525b8353d44375ae21
geekhunger <hello@geekhunger.com>
2023-12-06 23:18:28 +0100

    rewrote almost everything, spotted and fixed logical errors, improved code, etc

--- dictionary.js
+++ dictionary.js
@@ -4,31 +4,33 @@
     For more information, inspect the `./translations.json` file.
 */
 
-import {assert, type} from "type-approve"
+import {assert, check, add as type} from "type-approve"
+
+type("language_alpha2", "languages_alpha2", value => /^[a-z]{2,2}$/.test(value))
 
 export const DICTIONARY = {}
 
 export const has = function(locale, identifier) { // check if dictionary contains a translation for given identifier and locale
-    assert(type({object: DICTIONARY}), "Missing a dictionary object!")
-    assert(type({strings: [locale, identifier]}), `Can't check dictionary for identifier '${identifier}' at locale '${locale}' because arguments are malformed!`)
+    assert(check({object: DICTIONARY}), "Missing a dictionary object!")
+    assert(check({strings: [locale, identifier], language_alpha2: locale}), `Can't check dictionary with identifier '${identifier}' and locale '${locale}' because of malformed values!`)
     return (
         DICTIONARY.hasOwnProperty(identifier) &&
         DICTIONARY[identifier].hasOwnProperty(locale) &&
-        type({string: DICTIONARY[identifier][locale]})
+        check({string: DICTIONARY[identifier][locale]})
     )
 }
 
 export const add = function(locale, identifier, text, override = false) {
-    assert(type({object: DICTIONARY}), "Missing a dictionary object!")
-    if(type({nils: [identifier, text], object: locale})) {
+    assert(check({object: DICTIONARY}), "Missing a dictionary object!")
+    if(check({nils: [identifier, text], object: locale})) {
         for(let [name, translations] of Object.entries(translations)) {
             for(let [language, translation] of Object.entries(value)) {
                 add(language, name, translation) // recursive call (will also check for existing entries)
             }
         }
     } else {
-        assert(override === true || !has(locale, identifier), `Translation with identifier '${identifier}' and locale '${locale}' already exists!`)
-        assert(type({string: text}), `Can't extend dictionary for identifier '${identifier}' on locale '${locale}' with text '${text}' because of malformed values!`)
+        assert(override === true || !has(locale, identifier), `Dictionary entry with identifier '${identifier}' and locale '${locale}' already exists!`)
+        assert(check({string: text}), `Can't extend dictionary for identifier '${identifier}' on locale '${locale}' with text '${text}' because of malformed values!`)
         DICTIONARY[identifier][locale] = text // save new entry
     }
 }
--- translator.js
+++ translator.js
@@ -1,61 +1,55 @@
 import {assert, type} from "type-approve"
 import {DICTIONARY, add} from "./dictionary.js"
 
+
 try {
     add({
-        missing_translation: {
-            en: "<Error: Missing english translation>",
-            de: "<Fehler: Keine deutsche Übersetzung>",
-            ru: "<Ошибка: Русский перевод не найден>"
-        }
-    })
-    add({
-        missing_locale: {
-            en: "<Error: Language with code '$1' is not found>",
-            de: "<Fehler: Die Sprache mit dem Code '$1' wurde nicht gefunden>",
-            ru: "<Ошибка: Язык с кодом '$1' не найден>"
-        }
-    })
-    add({
-        missing_placeholder: {
-            en: "<Error: Missing placeholder '$1'>",
-            de: "<Fehler: Platzhalter '$1' fehlt>",
-            ru: "<Ошибка: Заполнитель '$1' отсутсвует>"
+        ["Missing Translation Error"]: {
+            en: "{Missing '$1' translation!}",
+            de: "{Übersetzung '$1' fehlt!}",
+            ru: "{Перевод '$1' не найден!}"
         }
     })
 } catch(_) {
 }
 
-export const strip = function(value) { // string trimming that works well with urls
-    assert(type({string: value}), "Can't trim a non-string value!")
-    return value
-        .toLowerCase()
-        .replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]+/g, "") // remove punctation (including unicode, excluding `\s`), for more information, see https://stackoverflow.com/questions/7576945/javascript-regular-expression-for-punctuation-international/7578937#7578937
-        .trim() // remove leading and trailing whitespaces
-        .replace(/\s+/g, "-") // replace inner whitespaces
-}
-
 
 export const patch = function(text, ...substitutions) { // Substitute text placeholders with actual values
-    return text.replace(/(\$(\d+))/g, function(match, placeholder, id) {
-        /*
-            For example, `patch("Welcome back, $1. There are $2 messages for you, $1.", "Eric", 2)`
-            compiles into "Welcome back, Eric. There are 2 messages for you, Eric."
-            See how `$1` got replaces multiple times with the first substitution argument? - Cool, eh?!
-        */
-        return substitutions[id - 1] || placeholder // replacement value or keep placeholder identifier
-    })
+    assert(
+        type({string: text, array: substitutions}),
+        "Missing placeholder or substitution values!"
+    )
+
+    const searchquery = /(\$(\d+))/g
+    const placeholders = text.match(searchquery) || []
+
+    assert(
+        placeholders.length !== substitutions.length,
+        `Missmatch between placeholders (${placeholders.length}) and substitutions (${substitutions.length})!`
+    )
+
+    return text.replace(searchquery, (match, placeholder, id) => substitutions[id - 1] || placeholder) // replacement value or keep placeholder identifier
+    /*
+        For example, `patch("Welcome back, $1. There are $2 messages for you, $1.", "Eric", 2)`
+        compiles into "Welcome back, Eric. There are 2 messages for you, Eric."
+        See how `$1` got replaces multiple times with the first substitution argument? - Cool, eh?!
+    */
 }
 
 
 export const translate = function(locale, identifier, ...substitutions) {
+    assert(
+        check({string: locale, language_alpha2: locale}),
+        `Translation locale '${locale}' must be a two-letter lowercase 'ISO-639-1 Alpha2' language code!`
+    )
+
     const translation = type({string: identifier}) && DICTIONARY.hasOwnProperty(identifier)
         ? DICTIONARY[identifier]
-        : DICTIONARY.missing_translation
-    
-    const text = translation.hasOwnProperty(locale)
+        : DICTIONARY["Missing Translation Error"]
+
+    const text = translation.hasOwnProperty(locale) && type({string: translation[locale]})
         ? translation[locale]
-        : patch(DICTIONARY.missing_locale.en, locale)
+        : patch(translation.en, locale)
 
     return patch(text, ...substitutions)
 }
