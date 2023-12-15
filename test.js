5bb94a5d88a2dc54638f4854fcf8c2b01f1dddeb
geekhunger <hello@geekhunger.com>
2023-12-15 12:51:32 +0100

    rewrote test file structure

--- test.js
+++ test.js
@@ -42,21 +42,183 @@ const REQUIRED_PROPERTIES = {
     ]
 }
 
+const UNIT_TESTS = { // tests for class properties
+    has: (t, name, has) => {
+        t.true(
+            type({function: has}),
+            `Method '${name}' must be a function!`
+        )
+        t.regex( // translation should be longer
+            t.throws(has.bind(this, "ab", "en"))?.message,
+            /invalid translation identifier/i,
+            "Translation ID must be invalid!"
+        )
+        t.regex( // translation with illegal characters should be invalid
+            t.throws(has.bind(this, "\n\t ", "en"))?.message,
+            /invalid translation identifier/i,
+            "Translation ID must be invalid!"
+        )
+        t.notThrows( // translation with whitelisted characters should be valid
+            has.bind(undefined, "123, Foo?-(Bar)_[B:A.Z]!", "en"),
+            "Translation ID must be valid!"
+        )
+        t.regex( // language code should be longer
+            t.throws(has.bind(this, "abc", "a"))?.message,
+            /invalid language code/i,
+            "Language must be invalid!"
+        )
+        t.regex( // language code should be shorter
+            t.throws(has.bind(this, "abc", "abc"))?.message,
+            /invalid language code/i,
+            "Language must be invalid!"
+        )
+        t.regex( // language code should be lowercase
+            t.throws(has.bind(this, "abc", "AA"))?.message,
+            /invalid language code/i,
+            "Language must be invalid!"
+        )
+        t.is( // translation should exists
+            has("Missing Translation Error", dict.PREFERRED_LANGUAGE),
+            true,
+            "Translation must exist!"
+        )
+        t.is( // translation should not exist
+            has("Foobar", "zz"),
+            false,
+            "Translation must not exist!"
+        )
+    },
+    add: (t, name, add) => {
+        t.true(
+            type({function: add}),
+            `Method '${name}' must be a function!`
+        )
+        t.regex( // should fail creating translation with malformed content
+            t.throws(add.bind(undefined, "Foobar", {foo: "bar"}, "en"))?.message,
+            /malformed translation value/i,
+            "Must fail because of malformed contents!"
+        )
+        t.is( // should be able to create new translation
+            add("Foobar", "Hello World", "en"),
+            "Hello World",
+            "Must be able to create translation!"
+        )
+        t.regex( // should not be able to override existing translation
+            t.throws(add.bind(undefined, "Foobar", "Hey", "en"))?.message,
+            /conflicting translation/i,
+            "Must conflict with existing translation!"
+        )
+        t.is( // should be able to force-override existing translation
+            add("Foobar", "Welcome", "en", true),
+            "Welcome",
+            "Must be able to override existing translation!"
+        )
+        t.notThrows(() => { // should not override the entire dictionary but work exactly as add()
+            dict.DICTIONARY = {
+                "Foo": {de: "foo$1"},
+                "Bar": {es: "$1bar"},
+                "Baz": {fr: "$1baz"}
+            }
+            t.assert(
+                dict.has("Foo", "de") &&
+                dict.has("Bar", "es") &&
+                dict.has("Baz", "fr") &&
+                dict.has("Missing Translation Error", dict.PREFERRED_LANGUAGE), // this should still exist
+                "Should not override dictionary but add new entries!"
+            )
+        })
+        t.regex( // should notify about language that is not present in all other translations
+            t.throws(() => {
+                delete dict.DICTIONARY["Missing Translation Error"].en
+                add("Foobar", "ups", "en", /*bypass checks of 'has' method:*/ true)
+                /*
+                    'Foo' translation exists and already has 'en' language
+                    but somehow not every translation contains the 'en' language
+                    (maybe through manual manipulation of dictionary),
+                    thus we try to insert a translation with a language
+                    that isn't yet fully supported by all available translations.
+                */
+            })?.message,
+            /reports other missing translations/i,
+            "Must notify about missing translations for an yet unknown language!"
+        )
+    },
+    patch: (t, name, patch) => {
+        t.true(
+            type({function: patch}),
+            `Method '${name}' must be a function!`
+        )
+        t.regex( // should error because of invalid value
+            t.throws(patch.bind(undefined, null))?.message,
+            /invalid value/i,
+            "Missing value with placeholders!"
+        )
+        t.regex( // should error because of missing substitution
+            t.throws(patch.bind(undefined, "Hello $1"))?.message,
+            /missmatch between placeholders/i,
+            "Missing substitution!"
+        )
+        t.regex( // should error because more placeholders than substitutions
+            t.throws(patch.bind(undefined, "$1 $2", "Hello"))?.message,
+            /missmatch between placeholders/i,
+            "Must have enough substitution to match all placeholders!"
+        )
+        t.regex( // should error because placeholder starts with $0 (should start with $1)
+            t.throws(patch.bind(undefined, "Hello $0", "World"))?.message,
+            /must start with \$?1/i,
+            "Placeholder count must start with 1!"
+        )
+        t.is( // should pass because values for substitution can be explicitly set to undefined or null
+            patch("$1 $2", "Hello", undefined, "World"),
+            "Hello $2",
+            "Should allow to skip placeholders by passing undefined or null!"
+        )
+    },
+    translate: (t, name, translate) => {
+        t.true(
+            type({function: translate}),
+            `Method '${name}' must be a function!`
+        )
+        t.is(
+            translate("Foo", "de", "bar"),
+            "foobar",
+            "Existing translations should work!"
+        )
+        t.is(
+            translate("Foobar", "de"),
+            "Übersetzung 'de' für 'Missing Translation Error' fehlt!",
+            "Missing translation should fallback onto default message but still use the given language!"
+        )
+        t.regex(
+            t.throws(translate.bind(undefined, "Foo", "es"))?.message,
+            /missing translations for '[a-z]{2,2}' on existing entries/i,
+            "Existing translation is missing given language (and missing primary language too)!"
+        )
+    }
+}
 
-test.serial("Validate setup of unit test", t => {
+
+test.serial("Validate test setup", t => {
+    // Validate setup of unit test
     const unassigned = REQUIRED_PROPERTIES.destructables.filter(name => !REQUIRED_PROPERTIES.functions.includes(name))
     t.true(unassigned < 1, "Found properties that aren't a subset of 'REQUIRED_PROPERTIES.functions': " + JSON.stringify(unassigned))
-})
 
-
-test.serial("Validate required class properties", t => {
+    // Validate required class properties
     const whitelist = [...new Set(Object.values(REQUIRED_PROPERTIES).flat())]
     const unknown = CLASS_PROPERTIES.filter(name => !whitelist.includes(name))
     t.is(unknown.length, 0, "Found untested and undocumented properties: " + JSON.stringify(unknown))
+
+    // Validate required tests for listed class properties
+    const untested = REQUIRED_PROPERTIES.destructables.filter(name => !Object.keys(UNIT_TESTS).includes(name))
+    t.is(untested.length, 0, "Found untested properties: " + JSON.stringify(untested))
+
+    const unimplemented = Object.keys(UNIT_TESTS).filter(name => !REQUIRED_PROPERTIES.destructables.includes(name))
+    t.is(unimplemented.length, 0, "Found tests for unknown properties: " + JSON.stringify(untested))
 })
 
 
-test.serial("Validate types of class properties", t => {
+test.serial("Validate class properties", t => {
+    // Validate types of class properties
     const getPropertyDescriptor = name => Object.getOwnPropertyDescriptor(Object.getPrototypeOf(dict), name)
     const hasGetter = property => type({function: getPropertyDescriptor(property).get})
     const hasSetter = property => type({function: getPropertyDescriptor(property).set})
@@ -72,10 +234,8 @@ test.serial("Validate types of class properties", t => {
     for(let name of REQUIRED_PROPERTIES.setters) {
         t.true(hasSetter(name), `Property '${name}' must have a getter and setter!`)
     }
-})
 
-
-test.serial("Validate default values of class properties", t => {
+    // Validate default values of class properties
     t.true(
         dict.AVAILABLE_LANGUAGES?.length >= 1,
         "There must be translations, hence there must be languages!"
@@ -99,171 +259,10 @@ test.serial("Validate default values of class properties", t => {
 })
 
 
-test("Destruct and unit-test class methods (incl. 'this' bindings)", t => {
-    const mockup = { // test class properties with the fallowing datasets
-        has: has => {
-            t.true(
-                type({function: has}),
-                "Method 'has' must be a function!"
-            )
-            t.regex( // translation should be longer
-                t.throws(has.bind(this, "ab", "en"))?.message,
-                /invalid translation identifier/i,
-                "Translation ID must be invalid!"
-            )
-            t.regex( // translation with illegal characters should be invalid
-                t.throws(has.bind(this, "\n\t ", "en"))?.message,
-                /invalid translation identifier/i,
-                "Translation ID must be invalid!"
-            )
-            t.notThrows( // translation with whitelisted characters should be valid
-                has.bind(undefined, "123, Foo?-(Bar)_[B:A.Z]!", "en"),
-                "Translation ID must be valid!"
-            )
-            t.regex( // language code should be longer
-                t.throws(has.bind(this, "abc", "a"))?.message,
-                /invalid language code/i,
-                "Language must be invalid!"
-            )
-            t.regex( // language code should be shorter
-                t.throws(has.bind(this, "abc", "abc"))?.message,
-                /invalid language code/i,
-                "Language must be invalid!"
-            )
-            t.regex( // language code should be lowercase
-                t.throws(has.bind(this, "abc", "AA"))?.message,
-                /invalid language code/i,
-                "Language must be invalid!"
-            )
-            t.is( // translation should exists
-                has("Missing Translation Error", dict.PREFERRED_LANGUAGE),
-                true,
-                "Translation must exist!"
-            )
-            t.is( // translation should not exist
-                has("Foobar", "zz"),
-                false,
-                "Translation must not exist!"
-            )
-        },
-        add: add => {
-            t.true(
-                type({function: add}),
-                "Method 'add' must be a function!"
-            )
-            t.regex( // should fail creating translation with malformed content
-                t.throws(add.bind(undefined, "Foobar", {foo: "bar"}, "en"))?.message,
-                /malformed translation value/i,
-                "Must fail because of malformed contents!"
-            )
-            t.is( // should be able to create new translation
-                add("Foobar", "Hello World", "en"),
-                "Hello World",
-                "Must be able to create translation!"
-            )
-            t.regex( // should not be able to override existing translation
-                t.throws(add.bind(undefined, "Foobar", "Hey", "en"))?.message,
-                /conflicting translation/i,
-                "Must conflict with existing translation!"
-            )
-            t.is( // should be able to force-override existing translation
-                add("Foobar", "Welcome", "en", true),
-                "Welcome",
-                "Must be able to override existing translation!"
-            )
-            t.notThrows(() => { // should not override the entire dictionary but work exactly as add()
-                dict.DICTIONARY = {
-                    "Foo": {de: "foo$1"},
-                    "Bar": {es: "$1bar"},
-                    "Baz": {fr: "$1baz"}
-                }
-                t.assert(
-                    dict.has("Foo", "de") &&
-                    dict.has("Bar", "es") &&
-                    dict.has("Baz", "fr") &&
-                    dict.has("Missing Translation Error", dict.PREFERRED_LANGUAGE), // this should still exist
-                    "Should not override dictionary but add new entries!"
-                )
-            })
-            t.regex( // should notify about language that is not present in all other translations
-                t.throws(() => {
-                    delete dict.DICTIONARY["Missing Translation Error"].en
-                    add("Foobar", "ups", "en", /*bypass checks of 'has' method:*/ true)
-                    /*
-                        'Foo' translation exists and already has 'en' language
-                        but somehow not every translation contains the 'en' language
-                        (maybe through manual manipulation of dictionary),
-                        thus we try to insert a translation with a language
-                        that isn't yet fully supported by all available translations.
-                    */
-                })?.message,
-                /reports other missing translations/i,
-                "Must notify about missing translations for an yet unknown language!"
-            )
-        },
-        patch: patch => {
-            t.true(
-                type({function: patch}),
-                "Method 'patch' must be a function!"
-            )
-            t.regex( // should error because of invalid value
-                t.throws(patch.bind(undefined, null))?.message,
-                /invalid value/i,
-                "Missing value with placeholders!"
-            )
-            t.regex( // should error because of missing substitution
-                t.throws(patch.bind(undefined, "Hello $1"))?.message,
-                /missmatch between placeholders/i,
-                "Missing substitution!"
-            )
-            t.regex( // should error because more placeholders than substitutions
-                t.throws(patch.bind(undefined, "$1 $2", "Hello"))?.message,
-                /missmatch between placeholders/i,
-                "Must have enough substitution to match all placeholders!"
-            )
-            t.regex( // should error because placeholder starts with $0 (should start with $1)
-                t.throws(patch.bind(undefined, "Hello $0", "World"))?.message,
-                /must start with \$?1/i,
-                "Placeholder count must start with 1!"
-            )
-            t.is( // should pass because values for substitution can be explicitly set to undefined or null
-                patch("$1 $2", "Hello", undefined, "World"),
-                "Hello $2",
-                "Should allow to skip placeholders by passing undefined or null!"
-            )
-        },
-        translate: translate => {
-            t.true(
-                type({function: translate}),
-                "Method 'translate' must be a function!"
-            )
-            t.is(
-                translate("Foo", "de", "bar"),
-                "foobar",
-                "Existing translations should work!"
-            )
-            t.is(
-                translate("Foobar", "de"),
-                "Übersetzung 'de' für 'Missing Translation Error' fehlt!",
-                "Missing translation should fallback onto default message but still use the given language!"
-            )
-            t.regex(
-                t.throws(translate.bind(undefined, "Foo", "es"))?.message,
-                /missing translations for '[a-z]{2,2}' on existing entries/i,
-                "Existing translation is missing given language (and missing primary language too)!"
-            )
-        }
-    }
-
-    const untested = REQUIRED_PROPERTIES.destructables.filter(name => !Object.keys(mockup).includes(name))
-    t.is(untested.length, 0, "Found untested properties: " + JSON.stringify(untested))
-
-    const unknown = Object.keys(mockup).filter(name => !REQUIRED_PROPERTIES.destructables.includes(name))
-    t.is(unknown.length, 0, "Found tests for unknown properties: " + JSON.stringify(untested))
-
+test("Test class properties", t => {
     for(let name of REQUIRED_PROPERTIES.destructables) {
-        const delegate = dict[name] // assign to variable, similar to conventional destructing operation `const {add} = dict`
-        const validate = mockup[name] // unit test handler
-        validate(delegate)
+        const delegate = dict[name] // assign to variable to test 'this' bindings, similar to conventional destructing operation `const {add} = dict`
+        const validate = UNIT_TESTS[name] // unit test handler
+        validate(t, name, delegate)
     }
 })
